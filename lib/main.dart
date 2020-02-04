25262af6b91bfc5a2ae2d7d1151eb39eff8d2a45
DreamsInCS <yoannierhermida@knights.ucf.edu>
2020-02-04 12:12:10 -0500

    Added basic home screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,19 @@ void main() {
 // Create a new text widget to show some text
 // on the screen
 var app = MaterialApp(
-  home: Text('Testing...!')
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('Let\'s See Images!')
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Icon(
+        Icons.add,
+        color: Colors.white,
+        size: 30.0
+      ),
+      onPressed: () => print('Button was pressed.')
+    )
+  )
 );
 
 // Take that widget and get it on the screen
---
+++ lib/src/app.dart
@@ -0,0 +1,9 @@
+// Import Flutter helper library
+import 'package:flutter/material.dart';
+
+// Create a class that will be our custom widget
+// that extends StatelessWidget base class
+
+
+// Must define build() method that returns the widgets
+// that the app widget will show
