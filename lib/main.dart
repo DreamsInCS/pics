0e58a212bfd332e6861023c54ffc2819cffcecbe
DreamsInCS <yoannierhermida@knights.ucf.edu>
2020-02-07 12:06:37 -0500

    Tutorial Flutter project complete.

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,5 @@ import 'src/app.dart';
 
 // Define a main() function to run when our app starts
 void main() {
-// Create a new text widget to show some text
-// on the screen
-
-// Take that widget and get it on the screen
-runApp(App());
+  runApp(App());
 }
--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -12,14 +12,28 @@ class ImageList extends StatelessWidget {
   Widget build(context) {
     return ListView.builder(
       itemCount: images.length,
-      itemBuilder: (context, int index) => Container(
-        margin: EdgeInsets.all(20.0),            // "all" sides of the Container element
-        child: Image.network(images[index].url),
+      itemBuilder: (context, int index) => buildImage(images[index])
+    );
+  }
+
+  // Name is not particularly special, made up!
+  // Helps make Widget build() less massive!
+  buildImage(ImageModel image) {
+    return Container(
+        margin: EdgeInsets.only(left: 20.0, top: 20.0, right: 20.0, bottom: 10.0),            // "all" sides of the Container element
+        child: new Column(
+          children: <Widget>[                    // <Widget> not required, but nice for annotation
+            Padding(
+              child: Image.network(image.url),
+              padding: EdgeInsets.only(bottom: 10.0)
+            ), 
+            Text(image.title)
+          ]
+        ),
         padding: EdgeInsets.all(20.0),
         decoration: BoxDecoration(
           border: Border.all(color: Colors.redAccent)
         )
-      )
-    );
+      );
   }
 }
